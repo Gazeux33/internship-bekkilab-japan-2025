41d294f663d6d3021c447df2ad39ef7a959d5590
Theo <thoecastillo@yahoo.com>
2025-04-25 14:27:16 +0900

    forward pass session4 backprop

--- Session4BackPropagation/app/Main.hs
+++ Session4BackPropagation/app/Main.hs
@@ -1,6 +1,53 @@
 module Main (main) where
 
 import Lib
+import Torch
+import qualified Torch.Functional.Internal as FI
+import qualified Torch.Functional as F
+
+type LinearLayer = (Tensor, Tensor)
+
+
+data Layer = LinearL LinearLayer | Tanh | Sigmoid deriving (Show)
+
+initLinearLayer :: Int -> Int -> LinearLayer
+initLinearLayer i o = (zeros' [i,o], zeros' [o])
+
+forwardLinear :: LinearLayer -> Tensor -> Tensor
+forwardLinear (w, b) x = FI.matmul x w + b
+
+forwardLayer :: Layer -> Tensor -> Tensor
+forwardLayer (LinearL ll) x = forwardLinear ll x
+forwardLayer Tanh       x = F.tanh x
+forwardLayer Sigmoid    x = F.sigmoid x
+
+sampleLayer :: Layer -> IO Layer
+sampleLayer (LinearL (w,b)) = do
+  w' <- randIO' (shape w)
+  b' <- randIO' (shape b)
+  return $ LinearL (w', b')
+sampleLayer act = return act
+
+sampleModel :: [Layer] -> IO [Layer]
+sampleModel = mapM sampleLayer
+
+forwardModel :: [Layer] -> Tensor -> Tensor
+forwardModel ls x0 = foldl (\x l -> forwardLayer l x) x0 ls
 
 main :: IO ()
-main = someFunc
+main = do
+  
+  let model =
+        [ LinearL (initLinearLayer 1 2)
+        , Tanh
+        , LinearL (initLinearLayer 2 3)
+        , Sigmoid
+        ]
+
+  initializedModel <- sampleModel model
+  putStrLn $ map (show shape) model
+
+  inp <- randIO' [4,1]
+  let out = forwardModel initializedModel inp
+  putStrLn $ "shape of output: " ++ show (shape out)
+  putStrLn "Hello, Haskell!"
